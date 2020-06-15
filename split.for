7e491e94bb2b0005e47ca306ea474462fd3e6101
Yudai Konno <59801841+YudaiKonno@users.noreply.github.com>
2020-06-16 00:39:45 +0900

    文字列の分割slpitとfor
    文字列の分割slpitとforを使用して
    入力される文字列の分割表示。

---
+++ split.for
@@ -0,0 +1,17 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String line_str = sc.nextLine();
+        
+        String[] line = line_str.split(" "); 
+        
+        for (String s : line ){
+            
+        System.out.println(s);
+        } 
+    }
+}
