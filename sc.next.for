5ec575280c84baf14972b7ed4b0bc0343a33a00d
Yudai Konno <59801841+YudaiKonno@users.noreply.github.com>
2020-09-02 01:38:08 +0900

    代入された値の数だけ次に入力される文字列を改行して表示するプログラム
    最初に整数で値が入力される。※ここでは３
    ３個分の文字列が入力されるので、その文字列を改行して表示。
    
    ここでは英単語のdog,cat,pigが入力される。
    この３行分の文字列を改行して表示する。

---
+++ sc.next.for
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a);
+        for (int i =0;i < a + 1 ;i++ ){
+        String str = sc.nextLine();
+        System.out.println(str);
+            
+        } 
+    }
+}
