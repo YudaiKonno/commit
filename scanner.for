36640e5c75e3ea4574776a136f010ca4eb3bcd4f
Yudai Konno <59801841+YudaiKonno@users.noreply.github.com>
2020-09-12 18:34:40 +0900

    入力された複数行の数値表示プログラム
    入力された数値で最初に入力された整数を初期値として
    for文で【i】が初期値を下回る間入力された整数を改行して表示するプログラム。

---
+++ scanner.for
@@ -0,0 +1,15 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        int start = sc.nextInt();
+            for (int i = 0;i < start ;i++ ){
+                int a =sc.nextInt();
+                System.out.println(a);
+            } 
+    }
+}
